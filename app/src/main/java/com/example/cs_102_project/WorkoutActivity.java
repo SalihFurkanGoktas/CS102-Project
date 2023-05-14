cb66cb41bd093ead8a4fc3456854cddfcc9f8962
Furkan Goktas <furkangoktas@ug.bilkent.edu.tr>
2023-05-14 11:46:51 +0300

    Initial progress on app layout
    EoM

---
+++ app/src/main/java/com/example/cs_102_project/WorkoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs_102_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
