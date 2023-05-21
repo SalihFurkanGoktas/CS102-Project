7660455af0e641792b53dce9b8e61a831e8ffa82
Furkan Goktas <furkangoktas@ug.bilkent.edu.tr>
2023-05-21 15:19:34 +0300

    Initial implementation of realtime database

--- app/src/main/java/com/example/cs_102_project/MainActivity.java
+++ app/src/main/java/com/example/cs_102_project/MainActivity.java
@@ -6,6 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +22,38 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton streakSwitch;
     private ImageButton settingsSwitch;
 
+    private DatabaseReference testDataRef;
+    private TextView testText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavButtonManagement();
+
+
+        testDataRef = FirebaseDatabase.getInstance("https://cs102-73984-default-rtdb.europe-west1.firebasedatabase.app/").getReference("testNum");
+        testText = findViewById(R.id.testTextBox);
+
+
+        replaceText();
+
+    }
+
+    private void replaceText() {
+        testDataRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot snapshot) {
+                
+                String textValue = snapshot.getValue(String.class);
+                testText.setText(textValue;
+            }
+
+            @Override
+            public void onCancelled( DatabaseError error) {}
+        });
     }
 
 
