5a4e10e5097f5ed625c1343b231c4675eb294c8d
Furkan Goktas <furkangoktas@ug.bilkent.edu.tr>
2023-05-29 23:37:39 +0300

    map continuation

---
+++ app/src/main/java/com/example/cs_102_project/map_mainGym.java
@@ -0,0 +1,28 @@
+package com.example.cs_102_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class map_mainGym extends Fragment {
+
+
+
+    public map_mainGym() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map_main_gym, container, false);
+    }
+}
